e1d9fabb0c254172cd872d12bc635f1ffdf1c9c4
Scott Vickers <svickers@weareratio.com>
2016-06-15 16:07:36 -0700

    complete the bresenham algorithm

--- src/drawLine/bresenham.js
+++ src/drawLine/bresenham.js
@@ -1,17 +1,70 @@
+function incY(y) {
+    return y + 1
+}
+function incX(x) {
+    return x + 1
+}
+function decY(y) {
+    return y - 1
+}
+function decX(x) {
+    return x - 1
+}
+
 export default function drawLine(x1, y1, x2, y2) {
     let dx = x2 - x1
     let dy = y2 - y1
+
+    let stepY = incY
+    let stepX = incX
+
+    let absDx = Math.abs(dx)
+    let absDy = Math.abs(dy)
+
+    // bresenham assumption #1: 0 <= m <= 1
+    // slope is greater than 45deg, swap the axes
+    let steep = absDx < absDy
+    if (steep) {
+        let tempAbsDx = absDx
+        let tempdx = dx
+        let tempx1 = x1
+        let tempx2 = x2
+        absDx = absDy
+        dx = dy
+        x1 = y1
+        x2 = y2
+        absDy = tempAbsDx
+        dy = tempdx
+        y1 = tempx1
+        y2 = tempx2
+    }
+
+    // bresenham assumption #2: x2 > x1 && y2 > y1
+    // always plot the absolute values, flipping which direction to increment
+    if (dx < 0) {
+        dx = absDx
+        stepX = decX
+    }
+    if (dy < 0) {
+        dy = absDy
+        stepY = decY
+    }
 
     let Pi = 2 * dy - dx
     let line = []
     line.push([x1, y1])
     for (let i = 0; i < dx; i++) {
+        x1 = stepX(x1)
         if (Pi <= 0) {
             Pi = Pi + 2 * dy
-            line.push([++x1, y1])
         } else {
+            y1 = stepY(y1)
             Pi = Pi + 2 * dy - 2 * dx
-            line.push([++x1, ++y1])
+        }
+        if (steep) {
+            line.push([y1, x1])
+        } else {
+            line.push([x1, y1])
         }
     }
     return line
