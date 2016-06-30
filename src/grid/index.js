6f1e07d353970705694cbb2b54764b1ba75b9b54
Scott Vickers <svickers@weareratio.com>
2016-06-29 17:29:30 -0700

    import graphlib library

--- src/grid/index.js
+++ src/grid/index.js
@@ -2,13 +2,32 @@ import _ from 'lodash'
 import { Graph } from 'graphlib'
 
 export default class Grid {
-    constructor(size) {
+    constructor(rows, columns) {
         if (_.isInteger(size) && size >= 0) {
             this.size = size
         } else {
             this.size = 10
         }
 
+        this._graph = new Graph()
 
+        initialize_grid.call(this, rows, columns)
+    }
+
+    toString() {
+        return console.log(this._graph)
     }
 }
+
+function initialize_grid(rows, columns) {
+    for (let row of _.range(rows)) {
+        for (let column of _.range(columns)) {
+            this._graph.addVertex({
+                '@@vertexId': `${row}${column}`,
+                row,
+                column
+            })
+        }
+    }
+}
+
